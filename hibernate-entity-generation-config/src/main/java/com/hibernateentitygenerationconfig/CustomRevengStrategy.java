b594b7adb854254898e55366cd98022d422562eb
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2023-03-17 00:56:38 +0100

    Add automatic entity generation
    Add Hibernate tools Maven plugin for JPA entity generation
    and define custom configuration in CustomRevengStrategy class

---
+++ hibernate-entity-generation-config/src/main/java/com/hibernateentitygenerationconfig/CustomRevengStrategy.java
@@ -0,0 +1,38 @@
+package com.hibernateentitygenerationconfig;
+
+import org.hibernate.tool.api.reveng.TableIdentifier;
+import org.hibernate.tool.internal.reveng.strategy.DefaultStrategy;
+
+public class CustomRevengStrategy extends DefaultStrategy {
+
+    @Override
+    public boolean excludeTable(TableIdentifier ti) {
+        return ti.getName().equals("flyway_schema_history");
+    }
+
+    @Override
+    public String tableToClassName(TableIdentifier ti) {
+        return singularize(this.toUpperCamelCase(ti.getName()));
+    }
+
+    private String singularize(String name) {
+        String result = name;
+
+        String lower = name.toLowerCase();
+        if (lower.endsWith("ies")) {
+            // Inverse of pluralization rule: If it ends with y with a consonant just before it, change the y to ies
+            // cities --> city
+            result = name.substring(0, name.length() - 3) + "y";
+        } else if (lower.endsWith("ses") || lower.endsWith("xes") || lower.endsWith("zes") || lower.endsWith("shes") || lower.endsWith("ches")) {
+            // Inverse of pluralization rule: If it ends with s,x,z,ch or sh, end it with es
+            // switches --> switch or buses --> bus
+            result = name.substring(0, name.length() - 2);
+        } else if (lower.endsWith("s")) {
+            // Inverse of regular plural inflection
+            // customers --> customer
+            result = name.substring(0, name.length() - 1);
+        }
+
+        return result;
+    }
+}
